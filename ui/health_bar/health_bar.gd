f883abe1a085954703297fe67f28a7a0582db1a7
CheckmateChris1 <checkdoesgames@gmail.com>
2025-10-24 15:52:32 -0700

    Changed health bar to work with built-in value property
    Instead of needing to call set_health() as defined within the code, directly setting the value of the ProgressBar now works fine.

--- ui/health_bar/health_bar.gd
+++ ui/health_bar/health_bar.gd
@@ -5,11 +5,11 @@ var progress_stylebox: StyleBoxFlat = StyleBoxFlat.new()
 func _ready() -> void:
 	progress_stylebox.set_corner_radius_all(6)
 	add_theme_stylebox_override("fill", progress_stylebox)
-	set_health(value) # Updates initial colors
+	update_color(value) # Updates initial colors
 
-func set_health(health: int) -> void:
-	self.value = health
-	var green = min(255, (255/50)*(health)) # i used desmos to find the right line
-	var red = min(255, (-255/50)*(health)+510) # same here
+func update_color(value: float) -> void:
+	print("test")
+	var green = min(255, (255/50)*(value)) # i used desmos to find the right line
+	var red = min(255, (-255/50)*(value)+510) # same here
 	
 	progress_stylebox.bg_color = Color.from_rgba8(red,green,0,255)
