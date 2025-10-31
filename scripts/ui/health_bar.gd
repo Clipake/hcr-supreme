fdc6a0fcca0407d60301bf8dc7608871e6354d3f
CheckmateChris1 <checkdoesgames@gmail.com>
2025-10-31 16:56:07 -0700

    Fixed signal bus not updating value, only color

--- scripts/ui/health_bar.gd
+++ scripts/ui/health_bar.gd
@@ -4,17 +4,18 @@ var progress_stylebox: StyleBoxFlat = self.get_theme_stylebox('fill')
 
 func _ready() -> void:
 	# Connecting to signal bus
-	Events.set_player_health.connect(update_color)
+	Events.set_player_health.connect(set_health)
 	
 	# Setting initial health bar style
 	add_theme_stylebox_override("fill", progress_stylebox)
-	update_color(value) # Updates initial colors
+	set_health(value) # Updates initial colors
 
-func update_color(value: float) -> void:
+func set_health(value: float) -> void:
 	"""
 	Calculates health bar color from current value
 	:param value: The new health value
 	"""
+	self.value = value
 	var green = min(255, (255/50)*(value)) # i used desmos to find the right line
 	var red = min(255, (-255/50)*(value)+510) # same here
 	
