ebfcaa8954fbaa1e9f5a177dcf1d4e48166be77b
CheckmateChris1 <checkdoesgames@gmail.com>
2025-10-31 16:43:44 -0700

    Connected health UI to signal bus & reorganized

--- scripts/ui/health_bar.gd
+++ scripts/ui/health_bar.gd
@@ -1,13 +1,20 @@
 extends ProgressBar
 
-var progress_stylebox: StyleBoxFlat = StyleBoxFlat.new()
+var progress_stylebox: StyleBoxFlat = self.get_theme_stylebox('fill')
 
 func _ready() -> void:
-	progress_stylebox.set_corner_radius_all(6)
+	# Connecting to signal bus
+	Events.set_player_health.connect(update_color)
+	
+	# Setting initial health bar style
 	add_theme_stylebox_override("fill", progress_stylebox)
 	update_color(value) # Updates initial colors
 
 func update_color(value: float) -> void:
+	"""
+	Calculates health bar color from current value
+	:param value: The new health value
+	"""
 	var green = min(255, (255/50)*(value)) # i used desmos to find the right line
 	var red = min(255, (-255/50)*(value)+510) # same here
 	
