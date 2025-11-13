b9461af894a68473c18508c4e014c9fd48882b50
Christopher Walden <checkdoesgames@gmail.com>
2025-11-12 23:25:58 -0800

    Animation relies on _process instead of timer

--- scripts/ui/heart_overlay.gd
+++ scripts/ui/heart_overlay.gd
@@ -1,20 +1,20 @@
 extends Control
 
-@export var max_move_y: int = 5 ## Maximum amount a heart can move upwards in pixels every 0.01s
-@export var max_move_x: int = 5 ## Maximum amount a heart can move sideways in pixels every 0.01s
+@export var max_move_y: float = 5 ## Maximum upwards heart movement (unknown units)
+@export var max_move_x: float = 5 ## Maximum sideways heart movement (unknown units)
+
+var go_right: bool = randf() > 0.5 # Some hearts go left, others right
+var move_x: float = randf_range(0, max_move_x) if go_right else randf_range(0, -max_move_x)
 
 # Does heart animation on instantiation
-func _ready() -> void:	
-	var go_right: bool = randf() > 0.5
-	var move_x: float = randf_range(0, max_move_x) if go_right else randf_range(0, -max_move_x)
-	
-	while modulate.a >= 0:
-		position.y -= max_move_y
-		position.x += move_x
-		scale = calc_scale(modulate.a)
-		modulate = Color(modulate.r, modulate.g, modulate.b, modulate.a-0.01) # -1% opacity
-		$"DisappearTimer".start()
-		await $"DisappearTimer".timeout
+func _process(delta: float) -> void:
+	var time_factor: float = delta*100 # adjust for framerate
+	if modulate.a <= 0:
+		queue_free()
+	position.y -= max_move_y*time_factor
+	position.x += move_x*time_factor
+	scale = calc_scale(modulate.a*time_factor)
+	modulate = Color(modulate.r, modulate.g, modulate.b, modulate.a-(0.01*time_factor)) # -1% opacity
 
 
 # Calculates the scale based on current alpha
@@ -23,4 +23,3 @@ func calc_scale(alpha: float):
 		return scale+Vector2(0.05, 0.05)
 	else:
 		return scale-Vector2(0.015, 0.015)
-		
