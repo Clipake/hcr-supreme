5c6e003db438fe62be8cb6a15a587dbae9f45b22
Christopher Walden <checkdoesgames@gmail.com>
2025-11-16 16:36:29 -0800

    Restored main.tscn & reverted some changes
    I switched Yao's code from get_tree().paused to signals, however I was wrong (oops). Sorry Yao, your code was good before I sped through it. Reverted my changes.

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -1,26 +1,18 @@
 extends Control
 
-var game_paused: bool = false
-
-func resume():
-	Events.pause_game.emit()
-
-func paused():
-	Events.unpause_game.emit()
+func toggle_pause():
+	get_tree().paused = not get_tree().paused
+	visible = not visible
 	
-func testEsc():
+func _process(delta):
 	if Input.is_action_just_pressed('game_pause'):
-		resume() if game_paused else paused()
+		toggle_pause()
 
 func _on_resume_pressed():
-	resume()
+	toggle_pause()
 
 func _on_restart_pressed():
-	resume()
-	get_tree().reload_current_scene()
+	Events.restart_game.emit()
 
 func _on_quit_pressed():
 	Events.end_game.emit()
-
-func _process(_delta):
-	testEsc()
