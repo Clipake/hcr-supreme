60d3d1f034ed4fa8eaf716e4a00bedf25be3dd11
Clipake <tz.godspeed@gmail.com>
2025-11-18 12:35:30 -0800

    tung tung spawns by itself

--- scripts/interactables/spawning.gd
+++ scripts/interactables/spawning.gd
@@ -6,20 +6,30 @@ extends Node3D
 @export var DISTANCE = 10
 
 var spawn_locations
-var interactables = []
+var interactables = {}
 
 var spawn_amount_chances = {
 	1: 1/3.0,
 	2: 1/4.0,
-	3: 1/10.0,
-	0: 1/20.0
+	3: 1/20.0,
+	0: 1/25.0
 }
+
+var interactable_spawn_chances = {
+	"coin": 1/3.0,
+	"petr_sticker": 1/5.0,
+	"scooter": 1/7.0,
+	"six_seven": 1/7.0,
+	"tung_tung": 1/10.0,
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for file in DirAccess.get_files_at("res://scenes/interactables"):
 		if (file.get_extension() == 'import'):
 			file = file.replace('.import', '')
-		interactables.append(load('res://scenes/interactables/'+file))
+		interactables[file] = load('res://scenes/interactables/'+file)
+		
 	print(interactables)
 	print('run')
 	
@@ -52,18 +62,26 @@ func _on_timer_timeout() -> void:
 			num_obstacles = val
 			break
 	var available = [0, 1, 2]
-	
+	print(num_obstacles)
+	var chosen_interactables = []
 	for i in range(num_obstacles):
-		var interactable_index = randi_range(0, len(interactables)-1)
-		var interactable_scene = interactables[interactable_index]
+		var keys = interactables.keys()
+		var interactable_index = randi_range(0, len(keys)-1)
+		chosen_interactables.append(keys[interactable_index])
+		
+	if 'tung_tung.tscn' in chosen_interactables:
+		chosen_interactables = ['tung_tung.tscn']
+	
+	for chosen_interactable in chosen_interactables:
+		var interactable_scene = interactables[chosen_interactable]
 		var interactable = interactable_scene.instantiate()
+		
 
 		var location_index = randi_range(0, len(available)-1)
 		interactable.init(spawn_locations[available[location_index]].global_position, self)
+		add_child(interactable)
 
 		available.remove_at(location_index)
-		add_child(interactable)
 		
-	pass # Replace with function body.
 
 		
