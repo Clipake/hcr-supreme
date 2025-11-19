516640e9db00b225eac6981c4a6e4978012bec52
KennethSun1 <kennets3@uci.edu>
2025-11-19 14:41:43 -0800

    connected buttons to code

--- scripts/ui/start_screen.gd
+++ scripts/ui/start_screen.gd
@@ -3,10 +3,6 @@ extends Control
 func _ready() -> void:
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_start_game() -> void:
 	Events.start_game.emit()
 func _on_open_shop() -> void:
