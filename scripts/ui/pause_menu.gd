3dce6b6cb3e78ea8960129d48e47e659f83fd352
Christopher Walden <checkdoesgames@gmail.com>
2025-11-19 11:58:26 -0800

    [WIP] Developing UI audio transition

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -1,8 +1,45 @@
 extends Control
 
+@export var menu_music: AudioStreamMP3
+@export var run_music: AudioStreamMP3
+@export var run_music_node: AudioStreamPlayer
+@export var menu_music_node: AudioStreamPlayer
+
+@export var transition_timer: Timer
+
 func toggle_pause():
 	get_tree().paused = not get_tree().paused
+	_transition()
 	visible = not visible
+	
+func _transition():
+	# Does the volume transition
+	var x = 0
+	while x < 1:
+		x += transition_timer.wait_time
+		_transition_function(x)
+		transition_timer.start()
+		print(run_music_node.volume_linear)
+	
+	
+	
+func _transition_function(x: float):
+	# Calculates the current volume for the transition at time x
+	run_music_node.stream_paused = false
+	menu_music_node.stream_paused = false
+	if x <= 0.5:
+		run_music_node.volume_linear = _volume_function(x)
+	elif x <= 1:
+		menu_music_node.volume_linear = _volume_function(x)
+		
+	if x >= 1:
+		run_music_node.stream_paused = true
+		run_music_node.volume_linear = 1.0
+		menu_music_node.stream_paused = true
+		menu_music_node.volume_linear = 1.0
+	
+func _volume_function(x: float): # Used to determine volume in transition
+	return pow(cos(PI*x), 2)
 	
 func _process(delta):
 	if Input.is_action_just_pressed('game_pause'):
