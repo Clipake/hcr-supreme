46c6f7c73ad291d8b57e14eda31a1f7476b2cea6
CheckmateChris1 <checkdoesgames@gmail.com>
2025-11-25 17:38:33 -0800

    One interactable.gd script, GameState.gd

--- scripts/interactables/interactable.gd
+++ scripts/interactables/interactable.gd
@@ -2,32 +2,20 @@ extends Node3D
 var root_node: Node3D
 var collected = false
 
-signal collected_signal(effect_type: String)
 
-@export var effect_type: String = "plsshower"
-
-func _ready():
-	pass
-	
 func init(start_pos: Vector3, passed_node: Node3D):
 	position = start_pos
 	root_node = passed_node
-	
+
+
 func _physics_process(_delta: float) -> void:
 	global_position += Vector3(0, 0, root_node.speed*_delta)
-			
-func _on_body_entered(body):
-	# Check if the player is the one touching
-	print("hi")
-	if body.is_in_group("player") and not collected:
-		collected = true
-		set_physics_process(false)
-		queue_free()  # Remove coin from scene
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player") and not collected:
 		collected = true
 		set_physics_process(false)
-		emit_signal("collected_signal", effect_type) 
+		Events.touched_interactable.emit(name)
+		print('Emitted: touched_interactable(' + str(name) + ')')
 		queue_free()  # Remove coin from scene
