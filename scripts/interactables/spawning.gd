8a4bc43e5654469dbe81af617197621a629f21aa
Christopher Walden <checkdoesgames@gmail.com>
2025-11-26 16:28:56 -0800

    Spawning & reel colors work

--- scripts/interactables/spawning.gd
+++ scripts/interactables/spawning.gd
@@ -2,47 +2,39 @@ extends Node3D
 
 @onready var spawn_locations: Array = $SpawnLocations.get_children()
 
-@export var ramp_up_speed: float = 0.001 ## The speed at which the game accelerates
+@export var ramp_up_speed: float = 0.1 ## The speed at which the game accelerates
 @export var distance: float = 20.0 ## Distance between reel tiles
 
+@export var bad_color: Color = Color(1.0, 0.5, 0.5)
+@export var good_color: Color = Color(0.5, 1.0, 0.5)
+@export var neutral_color: Color = Color(0.5, 0.5, 0.5)
+
 @export var interactables: Dictionary[PackedScene, int]
+@export var spawn_amount_chances: Dictionary[int, int] = {
+	0: 20, 1: 40, 2: 30, 3: 10
+}
 @export var reel_tile: PackedScene
 @export var run_music_node: AudioStreamPlayer
 @export var menu_music_node: AudioStreamPlayer
 
-var speed: float = 1 ## Measured as a multiplier from 1x being starting speed
 var time_elapsed: float = 0 ## Used as a timer for spawning
 
-var score := 0
-var coins := 0
-
-@export var interactable_spawn_chances := {
-	"coin": 40,
-	"petr_sticker": 20,
-	"scooter": 10,
-	"six_seven": 10,
-	"please_shower": 5,
-	"tung_tung": 5,
-	"jobapplication": 10
-}
-@export var spawn_amount_chances := {
-	1: 40, 2: 30, 3: 10, 0: 20,
-}
 
 func _ready() -> void:
 	_spawn_initial_tiles()
 	_start_run_music()
 
 func _physics_process(delta: float) -> void:
-	speed += delta * ramp_up_speed
-	time_elapsed += speed * delta
+	GameState.run_speed += delta * ramp_up_speed
+	time_elapsed += GameState.run_speed * delta
 	
-	if time_elapsed > 1:
+	# ngl, 3 just happens to make the reels spaced apart well, idk its significance
+	if time_elapsed > 3:
 		_on_timer_timeout()
 		time_elapsed = 0
 
 func _on_timer_timeout() -> void:
-	var num_obstacles = 3 #_get_weighted(spawn_amount_chances)
+	var num_obstacles = _get_weighted(spawn_amount_chances)
 	var chosen = []
 	
 	for i in num_obstacles:
@@ -75,26 +67,43 @@ func _get_weighted(items: Dictionary) -> Variant:
 	return null  # unreachable unless items is empty
 
 func _spawn_reel_row(items: Array):
-	# TODO add tung tung running side to side
-	for i in range(3):
-		_spawn_reel_tile(i, items[i])
+	# randomly pick which lanes get items
+	var lanes := [0, 1, 2]
+	lanes.shuffle()
+
+	var lane_to_item := [null, null, null]
+
+	# assign each item to a random lane
+	for i in range(items.size()):
+		lane_to_item[lanes[i]] = items[i]
+
+	# now spawn row using the mapped assignments
+	for lane in 3:
+		_spawn_reel_tile(lane, lane_to_item[lane])
 
 func _spawn_reel_tile(column: int, above: PackedScene = null):
+	# Finding init spawn position for this reel tile
 	var base_position = spawn_locations[column].global_position
 	var pos = base_position * Vector3(1.1, 1, 1)
 	
-	var tile = reel_tile.instantiate()
-
-	# simple color logic
 	var tile_color = Color(1, 1, 1)
 	if above:
-		if above.resource_name in ["jobapplication.tscn", "six_seven.tscn", "tung_tung.tscn"]:
-			tile_color = Color(1.0, 0.5, 0.5) # red-ish
-		elif above.resource_name in ["petr_sticker.tscn", "scooter.tscn"]:
-			tile_color = Color(0.5, 1.0, 0.5) # green-ish
+		# spawn above interactable
+		var interactable = above.instantiate()
+		interactable.init(pos)
+		add_child(interactable)
+		
+		# change reel tile color
+		var filename = above.resource_path.substr(above.resource_path.rfind('/')+1)
+		if filename in ["jobapplication.tscn", "six_seven.tscn", "tung_tung.tscn"]:
+			tile_color = bad_color
+		elif filename in ["petr_sticker.tscn", "scooter.tscn"]:
+			tile_color = good_color
 		else:
-			tile_color = Color(0.5, 0.5, 0.5) # neutral
+			tile_color = neutral_color
 	
+	# spawn reel tile
+	var tile = reel_tile.instantiate()
 	tile.init(pos + Vector3(0, -0.75, 0), tile_color)
 	add_child(tile)
 
